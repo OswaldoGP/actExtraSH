51d15d831f5788c20b64be9a3742cfad2be1bfff
OswaldoGP <holapp334@gmail.com>
2026-04-29 12:37:02 -0600

    Add files via upload

---
+++ actExtra.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# actividades extra -----
+echo "instalacion de Node con nvm...."
+apt  update && apt upgrade -y
+apt install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+source ~/.bashrc
+echo "version de nvm"
+nvm --version
+
+nvm install --lts
+echo "version de node"
+node -v
+npm -v
+
+
+echo "instalacion de lamp...."
+
+apt  update && apt upgrade -y
+echo "apache..."
+sudo apt install apache2 -y
+echo "mariadb..."
+apt install mariadb-server -y 
+echo "php..."
+apt install php libapache2-mod-php php-mysql -y
+
+echo "historial de instalacion log.txt ...."
+less /var/log/apt/history.log
+
